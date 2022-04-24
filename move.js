cc729de6f2b9be9b560e042ac3d8fd53b3dfb8a5
JangYunSeong <46878756+JangYunSeong@users.noreply.github.com>
2022-04-25 01:43:44 +0900

    2022-04-25 make main and movemodal

---
+++ move.js
@@ -0,0 +1,17 @@
+function makeMove(){
+    this.name;
+    this.tel;
+    this.date;
+    this.curpos;
+    this.movpos;
+    this.good; 
+};
+
+function Move(name,tel,date,curpos,movpos,good){
+    this.name = name;
+    this.tel = tel;
+    this.date = date;
+    this.curpos = curpos;
+    this.movpos = movpos;
+    this.good = good; 
+};
